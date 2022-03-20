01796fa12d014384ece8afa5816f1e609e932fd5
unknown <sjayashri@yahoo.com>
2022-03-20 18:25:09 +0530

    c2

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ document.querySelector('#push').onclick= function
 		alert ("Please Enter a Task")
 	}
 	else {
-		document.querySelector('#tasks').innerHTML
-		+= '
+		document.querySelector('#tasks').innerHTML += '
 			<div class="task">
 				<span id="taskname">
 					${document.querySelector ('#newtask input').value}
