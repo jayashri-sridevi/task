db32c8f69a256bdd19db88a4b81e63bd4bc95c5e
unknown <sjayashri@yahoo.com>
2022-03-20 18:23:32 +0530

    c2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-document.querySelector('#push').onclick= function(){
+document.querySelector('#push').onclick= function
+(){
 	if(document.querySelector('#newtask input').value.length == 0){
-		alert ("Please Enter a Tasl")
+		alert ("Please Enter a Task")
 	}
 	else {
 		document.querySelector('#tasks').innerHTML
