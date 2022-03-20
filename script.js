14c4b8fbc39dec6c7dca59f48dba5ca0f7eacd24
unknown <sjayashri@yahoo.com>
2022-03-20 23:20:41 +0530

    d12212222

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ document.querySelector('#push').onclick = function(){
             }
         }
 
-        var tasks = document.querySelectorAll(".task1");
+        var tasks = document.querySelectorAll(".task");
         for(var i=0; i<tasks.length; i++){
             tasks[i].onclick = function(){
                 this.classList.toggle('completed');
